2356ae79104158cd90040cd641cd76e19d7d3004
mark47546 <mark47546natthaphon@gmail.com>
2021-02-12 14:03:35 +0700

    add data to file

---
+++ src/resumeData.js
@@ -0,0 +1,116 @@
+let resumeData = {
+    'name': 'Natthaphon Rakprakobkij',
+    'role': 'Frontend Developer',
+    'telephoneNum': '0826719104',
+    'gmail': 'mark47546natthaphon@gmail.com',
+    'githubId': 'mark47546',
+    'roleDescription': 'I like to learn and create an applications with new technology, in my free time I prefer to spend time with music and cooking. ',
+    'socialLinks':[
+        {
+            'name':'github',
+            'url':'https://github.com/mark47546',
+            'className':'fa fa-github'
+        },
+        {
+            'name':'linkedin',
+            'url':'https://www.linkedin.com/in/natthaphon-rakprakobkij-71a388169/',
+            'className':'fa fa-linkedin'
+        }
+    ],
+    'aboutMe':'I am currently a third-year student in the department of Software and Knowledge Engineering at Kasetsart University. I believe that I am a hardworking person and has always been passionate to learn a new skill.',
+    'address': '25/214 Regent Home 25 Tiwanon Talatkwan Nontaburi 11000',
+    'education':[
+        {
+            'UniversityName':'Kasetsart University',
+            'specialization':'Third-year student',
+            'year':'2020'
+        }
+    ],
+    'experience':[
+        {
+            'EventName':'KU Startup',
+            'Role':'Web Developer',
+            'Start':'31/05/2019',
+            'End':'2/06/2019'
+        },
+        {
+            'EventName':'SKE SOS Camp',
+            'Role':'Teacher Assistant',
+            'Start':'24/06/2019',
+            'End': '26/06/2019'
+
+        },
+        {
+            'EventName':'Exceed Camp',
+            'Role':'Web Developer and Presenter of a project',
+            'Start':'9/07/2019',
+            'End': '15/07/2019'
+        },
+        {
+            'EventName':'CE TNI Engineering Camp',
+            'Role': 'Instructor',
+            'Start': '21/07/2019',
+            'End':'22/07/2019'
+        },
+
+    ],
+    'skillsDescription':'My skill',
+    'skills':[
+        {
+            'skillname':'Python',
+            'imgurl':'images/python.png'
+        },
+        {
+            'skillname':'Django',
+            'imgurl':'images/django.png'
+        },
+        {
+            'skillname':'HTML5 & CSS & JS',
+            'imgurl':'images/htmlcssjs.png'
+        },
+        {
+            'skillname':'ReactJS',
+            'imageurl':'images/react.png',
+        },
+        {
+            'skillname':'Docker',
+            'imageurl':'images/docker.png',
+        },
+        {
+            'skillname':'MongolDB',
+            'imageurl':'images/mongol.png',
+        },
+        {
+            'skillname':'SQL',
+            'imageurl':'images/sql.png',
+        },
+    ],
+    'portfolio':[
+        {
+            'name':'My Portfolio',
+            'description':'Django Responsive Portfolio Website',
+            'link':'https://github.com/mark47546/portfolio',
+            'imgurl':'images/portfolio/portfolio.png'
+        },
+        {
+            'name':'Telling',
+            'description':'Django Online Reading Web Application',
+            'link':'https://github.com/kidstylex/TELLING',
+            'imgurl':'images/portfolio/telling1.png'
+        },
+        {
+            'name':'Weather-memo',
+            'description':'Django Weather forecasting Web API',
+            'link':'https://github.com/patdpat/weather-memo',
+            'imgurl':'images/portfolio/weather_memo.png'
+        },
+        {
+            'name':'Evaluately',
+            'description':' Django web application project.',
+            'link':'https://github.com/mmookptr/evaluately.git',
+            'imgurl':'images/portfolio/evaluately.png'
+
+        }
+    ]
+}
+export default resumeData
