2ccb829eff5dd917c9b4cb6c0701db0895e6e631
mark47546 <mark47546natthaphon@gmail.com>
2021-02-12 14:00:10 +0700

    add contactus section

---
+++ src/components/ContactUs.js
@@ -0,0 +1,28 @@
+import React,{Component} from 'react';
+export default class ContactUs extends Component{
+    render(){
+        let resumeData = this.props.resumeData;
+        return(
+            <section id='contact'>
+                <div className='align-center'>
+                    <div className='row' >
+                            <div className='widget'>
+                                <h4>
+                                    Github:
+                                    {resumeData.githubId}
+                                </h4>
+                                <h4>
+                                    Contact:
+                                    {resumeData.telephoneNum}
+                                </h4>
+                                <h4>
+                                    Gmail:
+                                    {resumeData.gmail}
+                                </h4>
+                            </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
