fefb129bd6c045d9a5ef642d42f4d75e5aae927e
mark47546 <marknpr@Natthaphons-MacBook-Pro.local>
2022-08-16 18:08:45 +0700

    update project link

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,17 +12,17 @@ let resumeData = this.props.resumeData;
                         resumeData.portfolio && resumeData.portfolio.map((item)=>{
                         return(
                         <div className='columns portfolio-item'>
-                            <div className='item-wrap'>
-                                
-                                    <img src={item.imgurl} className='item-img'/>
-                                    <div className='overlay'>
-                                        <div className='portfolio-item-meta'>
-                                            <a href={item.link}>
-                                                <h5>{item.name}</h5>
-                                            </a>
+                            <div className='item-wrap' >
+                                <a href={item.link} key={item}>
+                                        <img src={item.imgurl} className='item-img'/>
+                                        <div className='overlay'>
+                                            <div className='portfolio-item-meta'>
+                                                
+                                                    <h5>{item.name}</h5>
+                                            </div>
                                         </div>
-                                    </div>
-                                    {/* <h5>{item.description}</h5> */}
+                                        {/* <h5>{item.description}</h5> */}
+                                </a>
                                 
                             </div>
                         </div>
